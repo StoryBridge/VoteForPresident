0a4fdcc0851d98569e6048af43423be5333abf69
zzennam <zzennam@naver.com>
2017-04-04 12:28:59 +0900

    Comiit test22222

--- VoteForPresident/Controllers/HomeController.cs
+++ VoteForPresident/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace VoteForPresident.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            Console.WriteLine("test");
+            Console.WriteLine("test22222");
             return View();
         }
     }
